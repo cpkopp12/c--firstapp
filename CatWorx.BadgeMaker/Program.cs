fea735d7333aee4f588f53ae2d8e079edbe9039a
cpkopp <cameronpkopp@gmail.com>
2023-04-25 17:27:56 -1000

    create employee class

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
   class Program
   {
+    static List<string> GetEmployees()
+    {
+        List<string> employees = new List<string>();
+        
+        while (true)
+        {
+            Console.WriteLine("Please enter a name: ");
+            string input = Console.ReadLine() ?? "";
+            if (input == "")
+            {
+                break;
+            }
+            Employee currentEmployee = new Employee(input, "smilth");
+            employees.Add(currentEmployee.GetFullName());    
+        }
+        return employees;
+    }
+
+    static void PrintEmployees(List<string> nameList) 
+    {
+        for (int i = 0; i < nameList.Count; i++)
+        {
+            Console.WriteLine(nameList[i]);
+        }
+    }
+
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+        List<string> employeeList = GetEmployees();
+        
+        PrintEmployees(employeeList);
+
     }
   }
 }
